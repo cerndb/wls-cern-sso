dd6faead42221b83452ce626eccea85273354667
lurodrig <lurodrig@cern.ch>
2015-04-29 08:51:40 +0200

    Sample application for testing the auth & authz processes

---
+++ sample/src/SimpleUserPrincipal.java
@@ -0,0 +1,36 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserPrincipals
+ */
+@WebServlet("/simpleUserPrincipal")
+public class SimpleUserPrincipal extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public SimpleUserPrincipal() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("Class: " + request.getUserPrincipal().getClass());
+		out.println("UserPrincipal: " + request.getUserPrincipal());
+		out.println("RemoteUser: " + request.getRemoteUser());
+	}
+
+}
---
+++ sample/src/SubjectPrincipals.java
@@ -0,0 +1,46 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.Principal;
+import java.util.Set;
+
+import javax.security.auth.Subject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import weblogic.security.Security;
+
+/**
+ * Servlet implementation class SubjectPrincipals
+ */
+@WebServlet("/subjectPrincipals")
+public class SubjectPrincipals extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SubjectPrincipals() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		Subject subject = Security.getCurrentSubject();
+		Set<Principal> principals = subject.getPrincipals();
+		for (Principal principal : principals) {
+			out.println("Class:" + principal.getClass());
+			out.println("Name:" + principal.getName());
+		}
+	}
+
+}
